63e7e70a883885d36a2d2a0abca7bdadc0a09037
JeramyMorrill <jeramy.morrill@JeramyMorrill.local>
2018-02-06 13:05:41 -0500

    adding gradient

---
+++ GradientMode.ino
@@ -0,0 +1,19 @@
+
+
+DEFINE_GRADIENT_PALETTE( heatmap_gp ) {
+    0,     0,  0,  0,
+  128,   255,  0,  0,
+  224,   255,255,  0,
+  255,   255,255,255 };
+
+DEFINE_GRADIENT_PALETTE( undersea_gp ) {
+    0,     0,  0,  0,
+  128,   255,  0,  0,
+  224,   255,255,  0,
+  255,   255,255,255 };
+
+void setupGradientMode(){
+  
+    
+}
+
--- bluetooth.ino
+++ bluetooth.ino
@@ -2,6 +2,7 @@
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
+
 //----------------------------------------------------
 #define BUFSIZE                        128
 #define VERBOSE_MODE                   true
@@ -19,6 +20,9 @@ void printHex(const uint8_t * data, const uint32_t numBytes);
 // the packet buffer
 extern uint8_t packetbuffer[];
 //----------------------------------------------------
+int32_t charid_string;
+int32_t charid_number;
+//----------------------------------------------------
 
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
@@ -26,70 +30,87 @@ boolean isConnected = false;
 
 void setupBLE(){
   
-  if ( !ble.begin(VERBOSE_MODE) ){
-    DEBUG_PRINT_LN(F("Failed to initialize BLE"));
-  }
-
+  if ( !ble.begin(VERBOSE_MODE) ){ DEBUG_PRINT_LN(F("Failed to initialize BLE")); }
+  // ---------------------------------------
   ble.verbose(false);
-  ble.setMode(BLUEFRUIT_MODE_DATA);
   DEBUG_PRINT_LN(F("setup ble"));
-  
+  // ---------------------------------------
+  ble.sendCommandCheckOK( F("AT+GATTADDSERVICE=uuid=0x1234") );
+  ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2345,PROPERTIES=0x08,MIN_LEN=1,MAX_LEN=6,DATATYPE=string,DESCRIPTION=string,VALUE=abc"), &charid_string);
+  ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x6789,PROPERTIES=0x08,MIN_LEN=4,MAX_LEN=4,DATATYPE=INTEGER,DESCRIPTION=number,VALUE=0"), &charid_number);
+  // ---------------------------------------
+  ble.reset();
+  // ---------------------------------------
+  ble.echo(false);
+  // ---------------------------------------
+  ble.setConnectCallback(connected);
+  ble.setDisconnectCallback(disconnected);
+  // ---------------------------------------
+  ble.setBleUartRxCallback(BleUartRX);
+  ble.setBleGattRxCallback(charid_string, BleGattRX);
+  ble.setBleGattRxCallback(charid_number, BleGattRX);
+  // ---------------------------------------
 }
 
-void onConnect(){
+void connected(){
   DEBUG_PRINT_LN(F("Connected"));
   DEBUG_PRINT_LN( F("Switching to DATA mode!") );
   ble.setMode(BLUEFRUIT_MODE_DATA);
+  isConnected = true;
 }
 
-void onDisconnect(){
+void disconnected(){
   DEBUG_PRINT_LN(F("Disconnected"));
-}
-
-void checkForConnection(){
-  if(ble.isConnected() && isConnected == false){
-    isConnected = true;
-    onConnect();
-  }else if(!ble.isConnected() && isConnected == true){
-    isConnected = false;
-    onDisconnect();
-  }
-}
-
-void checkForData(){
-  if(isConnected){
-    uint8_t len = readPacket(&ble, BLE_READPACKET_TIMEOUT);
-    if (len == 0) return;
-
-//     printHex(packetbuffer, len);
-//     DEBUG_PRINT_LN(packetbuffer[1]);
-     if(packetbuffer[1] == 'G'){
-        float x, y, z;
-        x = parsefloat(packetbuffer+2);
-        y = parsefloat(packetbuffer+6);
-        z = parsefloat(packetbuffer+10);
-        
-        Serial.print("Gyro\t");
-        Serial.print(x); Serial.print('\t');
-        Serial.print(y); Serial.print('\t');
-        Serial.print(z); Serial.println();
-     }
-     if (packetbuffer[1] == 'C') {
-      DEBUG_PRINT_LN( F("color sent") );
-      uint8_t red = packetbuffer[2];
-      uint8_t green = packetbuffer[3];
-      uint8_t blue = packetbuffer[4];
-      DEBUG_PRINT_LN(red);
-      DEBUG_PRINT_LN(green);
-      DEBUG_PRINT_LN(blue);
-
-      showColor(0,red,green,blue);
-     }
-  }
+  isConnected = false;
 }
 
 void BLELoop(){
-  checkForConnection();
-  checkForData();
+  ble.update(0);
+}
+
+void BleUartRX(char data[], uint16_t len){
+//  Serial.print( F("[BLE UART RX]" ) );
+//  Serial.write(data, len);
+//  Serial.println();
+
+  if (data[1] == 'C') {
+    uint8_t red = data[2];
+    uint8_t green = data[3];
+    uint8_t blue = data[4];
+    Serial.print ("RGB #");
+    if (red < 0x10) Serial.print("0");
+    Serial.print(red, HEX);
+    if (green < 0x10) Serial.print("0");
+    Serial.print(green, HEX);
+    if (blue < 0x10) Serial.print("0");
+    Serial.println(blue, HEX);
+  }
+  if (data[1] == 'B') {
+    uint8_t buttnum = data[2] - '0';
+    boolean pressed = data[3] - '0';
+    Serial.print ("Button "); Serial.print(buttnum);
+    if (pressed) {
+      Serial.println(" pressed");
+    } else {
+      Serial.println(" released");
+    }
+  }
+}
+
+void BleGattRX(int32_t chars_id, uint8_t data[], uint16_t len){
+  // ---------------------------------------
+  Serial.print( F("[BLE GATT RX] (" ) );
+  Serial.print(chars_id);
+  Serial.print(") ");
+  // ---------------------------------------
+  if (chars_id == charid_string){  
+    Serial.write(data, len);
+    Serial.println();
+  }else if (chars_id == charid_number){
+    int32_t val;
+    memcpy(&val, data, len);
+    Serial.println(val);
+  }
+  // ---------------------------------------
 }
 
